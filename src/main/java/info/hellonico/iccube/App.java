c58a551c7278c19f45f630ea1de7d30340ffa86f
Nicolas Modrzyk <hellonico@gmail.com>
2022-09-22 14:34:42 +0900

    fix conection url

--- src/main/java/info/hellonico/iccube/App.java
+++ src/main/java/info/hellonico/iccube/App.java
@@ -4,33 +4,31 @@ import org.olap4j.*;
 import org.olap4j.layout.RectangularCellSetFormatter;
 import org.olap4j.metadata.Member;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
 
 public class App {
+    
+    public static String mdxFileToStringQuery(String resourceFile) throws IOException {
+        URI sqlScriptUrl = URI.create(App.class.getResource(resourceFile).toExternalForm());
+        return Files.readString(Path.of(sqlScriptUrl));
+    }
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 
-        Class.forName("org.olap4j.driver.xmla.XmlaOlap4jDriver");
+        String url = "jdbc:xmla:Server=http://localhost:8282/icCube/xmla;Catalog=hello";
+        Connection connection =
+                DriverManager.getConnection(url, "admin", "admin");
 
-        final Connection connection = DriverManager.getConnection(
-
-                "jdbc:xmla:Server=http://localhost:8282/icCube/xmla;Catalog=hello",
-                "admin",
-                "admin"
-
-        );
-
-        final OlapConnection olapConnection = connection.unwrap(OlapConnection.class);
-
-        String query =
-                "SELECT {[Measures].[London Mean Background PM2.5 Particulate (ug/m3)]} ON \"value\" [DATE].[DATE].[Year].allmembers ON \"axis\" FROM [cube]";
-
-        final CellSet cellSet = olapConnection.createStatement().executeOlapQuery(query);
-
-        final RectangularCellSetFormatter formatter = new RectangularCellSetFormatter(false);
-
-        final PrintWriter writer = new PrintWriter(System.out);
+        OlapConnection olapConnection = connection.unwrap(OlapConnection.class);
+        CellSet cellSet = olapConnection.createStatement().executeOlapQuery(mdxFileToStringQuery("/airquality.mdx"));
+        RectangularCellSetFormatter formatter = new RectangularCellSetFormatter(false);
+        PrintWriter writer = new PrintWriter(System.out);
         formatter.format(cellSet, writer);
         writer.flush();
 
